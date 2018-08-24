f09eebb01d840b680df6a560eb5b6b158f123502
Michael O'Neil <cam8059@yahoo.com>
2018-08-24 12:37:35 -0400

    Created Data class, data auto scales to fit

---
+++ src/Data.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Data{
+		protected List<double[]> matrix;
+		protected int dataMax, dataMin, channels, dataPoints;
+		
+		public Data() {
+			
+		}
+		
+		protected void loadData(File f) {
+			Scanner scan;
+			try {
+				scan = new Scanner(f);
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+				return;
+			}
+			
+			matrix = new ArrayList<>();
+			while(scan.hasNextLine()) {
+				String line = scan.nextLine();
+				double[] rowValues = Arrays.stream(line.split(",")).mapToDouble(Double::parseDouble).toArray();
+				matrix.add(rowValues);
+			}
+			scan.close();
+			transpose();
+			dataMax = getMaxValue(matrix);
+			dataMin = getMinValue(matrix);
+			channels = matrix.size();
+			dataPoints = matrix.get(0).length;
+		}
+		
+		private int getMaxValue(List<double[]> list) {
+			double max = Double.MIN_VALUE;
+			
+			for (double[] row : list)
+				for (double d : row)
+					max = Double.max(d, max);
+			
+			return ((int)max+1);
+		}
+		
+		private int getMinValue(List<double[]> list) {
+			double min = Double.MAX_VALUE;
+			
+			for (double[] row : list)
+				for (double d : row)
+					min = Double.min(d, min);
+			
+			return ((int)min-1);
+		}
+		
+		protected void transpose() {
+			List<double[]> n = new ArrayList<>();
+			int rows = matrix.get(0).length;
+			int cols = matrix.size();
+			
+			for (int i = 0; i < rows; i++) {
+				double[] row = new double[cols];
+				
+				//Populate row with i-th column values
+				for (int j = 0; j < matrix.size(); j++) 
+					row[j] = matrix.get(j)[i];
+				
+				n.add(row);
+			}
+			matrix = n;
+		}
+	}
--- src/DataTrimmerGUI.java
+++ src/DataTrimmerGUI.java
@@ -9,36 +9,27 @@ import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
 
 public class DataTrimmerGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private List<double[]> matrix = new ArrayList<>();
-	private int maxVal, minVal;
-	private VisibleWindow window = new VisibleWindow();
-	private JPanel centerPanel;
+	private JPanel contentPane, centerPanel;
 	private JScrollPane scrollPane;
-	private int dataPoints;
 	private double yScale=1, xScale=1;
+	private JPanel xPanel, yPanel;
+	private JLabel lblMaxX, lblMinX, lblScaleX, lblMaxY, lblMinY, lblScaleY;
+	private JSpinner maxX, minX, scaleX, maxY, minY, scaleY;
+	Data data = new Data();
 	
-	class VisibleWindow{
-		int maxX,maxY,minX,minY;
-		int xScale, yScale;
-		int x,y;
-	}
-
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -54,12 +45,11 @@ public class DataTrimmerGUI extends JFrame {
 
 	public DataTrimmerGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 650, 550);
+		setBounds(100, 100, 800, 625);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
 		centerPanel = new JPanel() {
 
 			private static final long serialVersionUID = 1L;
@@ -72,13 +62,15 @@ public class DataTrimmerGUI extends JFrame {
 				g.setFont(new Font("Arial", Font.PLAIN, 12));
 				g.setColor(Color.black);
 				
-				if (matrix.isEmpty()) {
+				if (data.matrix == null || data.matrix.isEmpty()) {
 					g.drawString("No Data Loaded", 50, 50);
 				}
 				else {
-					for (double[] channel : matrix) {
-						for (int i=0; i < channel.length-1; i++)
-							g.drawLine((int)(i/xScale), (int)((maxVal-(int)channel[i])/yScale), (int)((i+1)/xScale), (int)((maxVal-(int)channel[i+1])/yScale));
+					for (double[] channel : data.matrix) {
+						for (int i=0; i < channel.length-1; i++) {
+							g.drawLine((int)(i/xScale),     (int)((data.dataMax-channel[i])/yScale),
+									   (int)((i+1)/xScale), (int)((data.dataMax-channel[i+1])/yScale));
+						}
 					}
 				}
 				
@@ -102,42 +94,13 @@ public class DataTrimmerGUI extends JFrame {
 		btnLoadData.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Scanner scan;
-				try {
-					scan = new Scanner(new File("eeg.csv"));
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-					return;
-				}
 				
-				while(scan.hasNextLine()) {
-					String line = scan.nextLine();
-					double[] rowValues = Arrays.stream(line.split(",")).mapToDouble(Double::parseDouble).toArray();
-					matrix.add(rowValues);
-				}
-				scan.close();
-				
-				matrix = transpose(matrix);
-//				for (double[] d : matrix) 
-//					System.out.println(Arrays.toString(d));
-				
-				maxVal = getMaxValue(matrix);
-				minVal = getMinValue(matrix);
-				dataPoints = matrix.get(0).length;
-				System.out.println(maxVal + " " + minVal);
-				
-				//scaleToFitWindow();
-//				window.maxY = maxVal;
-//				window.minY = minVal;
-//				window.minX = 0;
-//				window.maxX = matrix.get(0).length;
-//				window.x = 0;
-//				window.y = 0;
-
+				data.loadData(new File("eeg.csv"));
+				scaleToFitWindow();
 				repaint();
-				centerPanel.setPreferredSize(new Dimension(matrix.get(0).length, maxVal));
+				//centerPanel.setPreferredSize(new Dimension(data.matrix.get(0).length, data.dataMax));
 				//Update Scroll bar UI -hacky, needs a better way
-				setSize(getWidth()+1, getHeight()+1);
+				//setSize(getWidth()+1, getHeight()+1);
 				
 			}
 		});
@@ -154,53 +117,70 @@ public class DataTrimmerGUI extends JFrame {
 			}
 		});
 		bottomPanel.add(btnScaleToFit);
+		
+		xPanel = new JPanel();
+		bottomPanel.add(xPanel);
+		xPanel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		lblMaxX = new JLabel("Max X:");
+		xPanel.add(lblMaxX);
+		
+		maxX = new JSpinner();
+		xPanel.add(maxX);
+		
+		lblMinX = new JLabel("Min X:");
+		xPanel.add(lblMinX);
+		
+		minX = new JSpinner();
+		xPanel.add(minX);
+		
+		lblScaleX = new JLabel("Scale X:");
+		xPanel.add(lblScaleX);
+		
+		scaleX = new JSpinner();
+		xPanel.add(scaleX);
+		
+		yPanel = new JPanel();
+		bottomPanel.add(yPanel);
+		yPanel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		lblMaxY = new JLabel("Max Y:");
+		yPanel.add(lblMaxY);
+		
+		maxY = new JSpinner();
+		yPanel.add(maxY);
+		
+		lblMinY = new JLabel("Min Y:");
+		yPanel.add(lblMinY);
+		
+		minY = new JSpinner();
+		yPanel.add(minY);
+		
+		lblScaleY = new JLabel("Scale Y:");
+		yPanel.add(lblScaleY);
+		
+		scaleY = new JSpinner();
+		yPanel.add(scaleY);
+	}
+	
+	private void updateSpinners(int xMax, int xMin, int xScale, int yMax, int yMin, int yScale ) {
+		maxX.setValue(xMax);
+		minX.setValue(xMin);
+		scaleX.setValue(xScale);
+		maxY.setValue(yMax);
+		minY.setValue(yMin);
+		scaleY.setValue(yScale);
 	}
 	
 	private void scaleToFitWindow() {
 		int height = scrollPane.getHeight()-20;
 		int width = scrollPane.getWidth()-20;
 		
-		yScale = ((double)maxVal)/height;
-		xScale = ((double)dataPoints)/width;
+		yScale = ((double)(data.dataMax-data.dataMin))/height;
+		xScale = ((double)data.dataPoints)/width;
 		
+		updateSpinners(data.dataPoints, 0, (int)Math.ceil(xScale), data.dataMax, data.dataMin, (int)Math.ceil(yScale));
 		centerPanel.setPreferredSize(new Dimension(width, height));
-	}
-	
-	private int getMaxValue(List<double[]> list) {
-		double max = Double.MIN_VALUE;
-		
-		for (double[] row : list)
-			for (double d : row)
-				max = Double.max(d, max);
-		
-		return ((int)max+1);
-	}
-	
-	private int getMinValue(List<double[]> list) {
-		double min = Double.MAX_VALUE;
-		
-		for (double[] row : list)
-			for (double d : row)
-				min = Double.min(d, min);
-		
-		return ((int)min-1);
-	}
-
-	private List<double[]> transpose(List<double[]> original) {
-		List<double[]> n = new ArrayList<>();
-		int rows = original.get(0).length;
-		int cols = original.size();
-		
-		for (int i = 0; i < rows; i++) {
-			double[] row = new double[cols];
-			
-			//Populate row with i-th column values
-			for (int j = 0; j < original.size(); j++) 
-				row[j] = original.get(j)[i];
-			
-			n.add(row);
-		}
-		return n;
 	}
 	
 }
