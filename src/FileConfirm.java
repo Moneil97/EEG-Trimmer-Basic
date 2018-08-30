46134e5da8975b220be126d080e8ca9d0bcf7e6c
Moneil <cam8059@yahoo.com>
2018-08-29 21:16:57 -0400

    Fixed Formatting

--- src/FileConfirm.java
+++ src/FileConfirm.java
@@ -40,87 +40,72 @@ public class FileConfirm extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		setLocationRelativeTo(parent);
-		{
-			JLabel lblTheSelectedFile = new JLabel("The Selected File Contains:");
-			lblTheSelectedFile.setFont(new Font("Tahoma", Font.PLAIN, 30));
-			contentPanel.add(lblTheSelectedFile, BorderLayout.NORTH);
-		}
-		{
-			JPanel panel = new JPanel();
-			contentPanel.add(panel, BorderLayout.SOUTH);
-			panel.setLayout(new GridLayout(0, 1, 0, 0));
-			{
-				JPanel panel_1 = new JPanel();
-				panel.add(panel_1);
-				{
-					channelsLabel = new JLabel(data.channels + "");
-					channelsLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-					panel_1.add(channelsLabel);
-				}
-				{
-					JLabel lblChannels = new JLabel("Channels");
-					lblChannels.setFont(new Font("Tahoma", Font.PLAIN, 20));
-					panel_1.add(lblChannels);
-				}
+		
+		JLabel lblTheSelectedFile = new JLabel("The Selected File Contains:");
+		lblTheSelectedFile.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		contentPanel.add(lblTheSelectedFile, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		contentPanel.add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		channelsLabel = new JLabel(data.channels + "");
+		channelsLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_1.add(channelsLabel);
+		
+		JLabel lblChannels = new JLabel("Channels");
+		lblChannels.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_1.add(lblChannels);
+
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
+		dataPointsLabel = new JLabel(data.dataPoints + "");
+		dataPointsLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_2.add(dataPointsLabel);
+		
+		JLabel lblDataPoints = new JLabel("Data Points");
+		lblDataPoints.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_2.add(lblDataPoints);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+		
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				selection = "OK";
+				FileConfirm.this.dispatchEvent(new WindowEvent(FileConfirm.this, WindowEvent.WINDOW_CLOSING));
 			}
-			{
-				JPanel panel_1 = new JPanel();
-				panel.add(panel_1);
-				{
-					dataPointsLabel = new JLabel(data.dataPoints + "");
-					dataPointsLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-					panel_1.add(dataPointsLabel);
-				}
-				{
-					JLabel lblDataPoints = new JLabel("Data Points");
-					lblDataPoints.setFont(new Font("Tahoma", Font.PLAIN, 20));
-					panel_1.add(lblDataPoints);
-				}
+		});
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+	
+		JButton btnTranspose = new JButton("Transpose");
+		btnTranspose.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				data.transpose();
+				dataPointsLabel.setText(data.dataPoints + "");
+				channelsLabel.setText(data.channels + "");
 			}
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						selection = "OK";
-						FileConfirm.this.dispatchEvent(new WindowEvent(FileConfirm.this, WindowEvent.WINDOW_CLOSING));
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+		});
+		buttonPane.add(btnTranspose);
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.setActionCommand("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				selection = "Cancel";
+				data.matrix = null;
+				FileConfirm.this.dispatchEvent(new WindowEvent(FileConfirm.this, WindowEvent.WINDOW_CLOSING));
 			}
-			{
-				JButton btnTranspose = new JButton("Transpose");
-				btnTranspose.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						data.transpose();
-						dataPointsLabel.setText(data.dataPoints + "");
-						channelsLabel.setText(data.channels + "");
-					}
-				});
-				buttonPane.add(btnTranspose);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						selection = "Cancel";
-						data.matrix = null;
-						FileConfirm.this.dispatchEvent(new WindowEvent(FileConfirm.this, WindowEvent.WINDOW_CLOSING));
-					}
-				});
-				buttonPane.add(cancelButton);
-			}
-		}
+		});
+		buttonPane.add(cancelButton);
 	}
 }
